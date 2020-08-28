9ab018e775d7285cef9988e30abbf58075c7573c
Jerry Lu <tingcl2@uci.edu>
2020-08-27 17:36:58 -0700

    added scrolling effects and fixed nav menu issue

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,4 +3,17 @@ $(document).ready(function(){
         $(this).toggleClass('open');
         $('.navbar').toggleClass('open');
     });
+    $('.navbar .nav-link').on('click', function(){
+        $('.menu-hamburger').removeClass('open');
+        $('.navbar').removeClass('open');
+    });
+    $('nav a[href*="#"]').on('click', function(){
+        $('html, body').animate({
+            scrollTop: $($(this).attr('href')).offset().top
+        }, 2000);
+    });
+    AOS.init({
+        easing: 'ease',
+        duration: 2000
+    });
 });

77ff20c4ae5227300c4d0a6dc2e3bb8dafc0637f
Jerry Lu <tingcl2@uci.edu>
2020-08-27 19:33:15 -0700

    disable scrolling when nav bar is active

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,15 +2,17 @@ $(document).ready(function(){
     $('.menu-hamburger').on('click', function(){
         $(this).toggleClass('open');
         $('.navbar').toggleClass('open');
+        $('body').toggleClass('open');
     });
     $('.navbar .nav-link').on('click', function(){
         $('.menu-hamburger').removeClass('open');
         $('.navbar').removeClass('open');
+        $('body').removeClass('open');
     });
     $('nav a[href*="#"]').on('click', function(){
         $('html, body').animate({
             scrollTop: $($(this).attr('href')).offset().top
-        }, 2000);
+        }, 1000);
     });
     AOS.init({
         easing: 'ease',
